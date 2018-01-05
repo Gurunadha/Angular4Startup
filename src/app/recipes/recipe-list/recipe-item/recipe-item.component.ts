195e93706e475e4b785a2a9f36c8300f4bcfc0c9
nghiangodinh <you@example.com>
2018-01-05 11:40:32 +0700

    update recipe to break down components and pass data between components

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from "./../../recipe.model";
+import { Component, OnInit, Input, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  selector: "app-recipe-item",
+  templateUrl: "./recipe-item.component.html",
+  styleUrls: ["./recipe-item.component.css"]
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelected() {
+    this.recipeSelected.emit();
   }
-
 }
