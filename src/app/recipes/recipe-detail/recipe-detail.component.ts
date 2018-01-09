c814e87de1e9e5758b58850a2edb1c4a8c8edc49
nghiangodinh <you@example.com>
2018-01-09 16:18:09 +0700

    create module routing seperately apply routing with children routes

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,20 +1,43 @@
 import { RecipeService } from "./../recipe.service";
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { OnDestroy } from "@angular/core/src/metadata/lifecycle_hooks";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: "app-recipe-detail",
   templateUrl: "./recipe-detail.component.html",
   styleUrls: ["./recipe-detail.component.css"]
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  recipe: Recipe;
+  id: number;
+  private paramSubscription: Subscription;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.paramSubscription = this.route.params.subscribe((params: Params) => {
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(["edit"], { relativeTo: this.route });
+    // this.router.navigate(["../", this.id, "edit"], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
   }
 }
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,5 @@
 import { Recipe } from "./../../recipe.model";
 import { Component, OnInit, Input } from "@angular/core";
-import { EventEmitter } from "@angular/core";
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: "app-recipe-item",
@@ -10,12 +8,7 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) {}
+  @Input() index: number;
 
   ngOnInit() {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
